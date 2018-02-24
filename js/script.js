68bb41181ee4174d179bd7df355b5330315f31c9
jhonasr <jhonalex0525@gmail.com>
2018-02-24 01:43:46 -0500

    uff

---
+++ js/script.js
@@ -0,0 +1,33 @@
+//form
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbyjQF8gPZVPK5EjhJ6sEfPbwe3IGDRqb5B7FGVaBzB1XnHAh1c/exec'
+  const form = document.forms['submit-to-google-sheet']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  })
+
+  // Example starter JavaScript for disabling form submissions if there are invalid fields
+  (function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
+//open tab
+  function openWin() {
+    window.open('https://www.google.com')
+  }
